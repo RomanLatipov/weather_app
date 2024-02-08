93519e7e0fe57563436b313a1193207140d2617b
RomanLatipov <romanlatipov@gmail.com>
2024-02-07 21:54:55 -0500

    created api call

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function findCity(location) {
+    const apiData = {
+		url: 'https://api.openweathermap.org/data/2.5/weather?q=',
+		city: location,
+		id: '&APPID=b206d80c92d9a53cdc890c68d85f783e',
+	}
+    const apiUrl = `${apiData.url}${apiData.city}${apiData.id}`
+    fetch(apiUrl)
+		.then((data) => data.json())
+		.then((weather) => generateHtml(weather))
+
+
     const generateHtml = (data) => {
 		console.log(data)
 		const html = `
@@ -11,5 +22,4 @@ function findCity(location) {
         const weatherDiv = document.querySelector('.Weather')
 		weatherDiv.innerHTML = html
     }
-    generateHtml(location);
 }
