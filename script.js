0bb0fc9e449c6ab58a737e98a5bf9705fc140ae9
RomanLatipov <romanlatipov@gmail.com>
2024-02-13 22:54:24 -0500

    made js file work with new ui

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 function findCity(location) {
-	const apiData = {
-		url: 'https://api.openweathermap.org/data/2.5/weather?q=',
-		city: location,
-		id: '&APPID=b206d80c92d9a53cdc890c68d85f783e',
-	}
-	const apiUrl = `${apiData.url}${apiData.city}${apiData.id}`
+	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=b206d80c92d9a53cdc890c68d85f783e`;
 	fetch(apiUrl)
 		.then((data) => data.json())
 		.then((weather) => generateHtml(weather))
 
-	const generateHtml = (data) => {
-		console.log(data)
-		const html = `
-			<div class="card font">
-			<div class="name">${data.name}</div>
-			<img src='https://api.openweathermap.org/img/w/${data.weather[0].icon}'style="width:20%">
-			<div class="details">
-				<span><b>Temperature:</b> ${((data.main.temp-273)*(9/5)+32).toFixed(2)}\xB0F</span><br>			
-				<span><b>Weather:</b> ${data.weather[0].description}</span><br>
-				<span><b>humidity:</b> ${data.main.humidity}%</span>
-			</div> </div>
-		`
-		const weatherDiv = document.querySelector('.Weather')
-		weatherDiv.innerHTML = html
-	}
+		const generateHtml = (data) => {
+			console.log(data)
+			const html = `
+				<div class="card">
+					<div class="semi-circle">
+						<div class="search">
+							<input type="text" id="inputField" placeholder="Enther city name" title="Type in a city">
+							<button class="button" onclick="findCity(document.getElementById('inputField').value)"><img src="./assets/search.png"></button>
+						</div>
+						<p class="city">${data.name}</p>
+						<p class="type">${data.weather[0].description}</p>
+						<div class="weather">
+							<img src="./assets/rain.png" class="weather-icon">
+						</div>
+					</div>
+					<div class="info">
+						<div>
+							<img src="./assets/humidity.png">
+							<p class="humidity">${data.main.humidity}%</p>
+						</div>
+						<p class="temp">${((data.main.temp-273)*(9/5)+32).toFixed(2)}°</p>
+						<div>
+							<img src="./assets/wind.png">
+							<p class="wind">${data.main.wind}</p>
+						</div>
+					</div>
+					<!-- <div id="forecast">
+					</div> -->
+				</div>
+			`
+			const weatherDiv = document.querySelector('.card')
+			weatherDiv.innerHTML = html
+		}
 }
