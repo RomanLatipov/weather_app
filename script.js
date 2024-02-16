b7dd07a2dbb85b55a071e20c9e1438c57fc97994
RomanLatipov <romanlatipov@gmail.com>
2024-02-15 22:11:26 -0500

    made Js file mush less complicated

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
 function findCity(location) {
 	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=b206d80c92d9a53cdc890c68d85f783e`;
-	fetch(apiUrl)
-		.then((data) => data.json())
-		.then((weather) => generateHtml(weather))
+    async function checkWeather(url) {
+        const reponse = await fetch(url);
+        var data = await reponse.json();
 
-		const generateHtml = (data) => {
-			console.log(data)
-			const html = `
-				<div class="card">
-					<div class="semi-circle">
-						<div class="search">
-							<input type="text" id="inputField" placeholder="Enther city name" title="Type in a city">
-							<button class="button" onclick="findCity(document.getElementById('inputField').value)"><img src="./assets/search.png"></button>
-						</div>
-						<p class="city">${data.name}</p>
-						<p class="type">${data.weather[0].description}</p>
-						<div class="weather">
-							<img src="./assets/rain.png" class="weather-icon">
-						</div>
-					</div>
-					<div class="info">
-						<div>
-							<img src="./assets/humidity.png">
-							<p class="humidity">${data.main.humidity}%</p>
-						</div>
-						<p class="temp">${((data.main.temp-273)*(9/5)+32).toFixed(2)}°</p>
-						<div>
-							<img src="./assets/wind.png">
-							<p class="wind">${data.main.wind}</p>
-						</div>
-					</div>
-					<!-- <div id="forecast">
-					</div> -->
-				</div>
-			`
-			const weatherDiv = document.querySelector('.card')
-			weatherDiv.innerHTML = html
-		}
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".type").innerHTML = data.weather[0].description.charAt(0).toUpperCase()+data.weather[0].description.slice(1);;
+        document.querySelector(".temp").innerHTML = ((data.main.temp-273)*(9/5)+32).toFixed(0)+"°";
+        document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML = data.wind.speed+" mph";
+    }
+    checkWeather(apiUrl);
 }
