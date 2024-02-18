db09bac3b05d3b84853afe92e7c598373940495a
RomanLatipov <romanlatipov@gmail.com>
2024-02-17 23:48:17 -0500

    ui now displays multiple weather types

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function findCity(location) {
 	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=b206d80c92d9a53cdc890c68d85f783e`;
     async function checkWeather(url) {
         const reponse = await fetch(url);
+        const weatherIcon = document.querySelector(".weather-icon");
         var data = await reponse.json();
 
         console.log(data);
@@ -11,6 +12,25 @@ function findCity(location) {
         document.querySelector(".temp").innerHTML = ((data.main.temp-273)*(9/5)+32).toFixed(0)+"°";
         document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
         document.querySelector(".wind").innerHTML = data.wind.speed+" mph";
+
+        if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "./assets/clear.png";
+        }
+        else if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "./assets/clouds.png";
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "./assets/rain.png";
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "./assets/drizzle.png";
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "./assets/mist.png";
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "./assets/snow.png";
+        }
     }
     checkWeather(apiUrl);
 }
