9b5aee9e38551906b5f77b1b1a715a0de25ed798
romanlatipov <romanlatipov@gmail.com>
2024-03-29 13:53:20 -0400

    make functional changes

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ async function findCity(location) {
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=b206d80c92d9a53cdc890c68d85f783e`;
     reponse = await fetch(forecastUrl);
     const forecast = (await reponse.json()).list.slice(0, 8);
+    // console.log(forecast);
 
     displayWeather(data);
-    presArr = [data.main.humidity, (data.rain === undefined) ? 0 : Math.round(data.rain["1h"]), Math.round(data.wind.speed)];
+    const presArr = [data.main.humidity, (data.rain === undefined) ? 0 : Math.round(data.rain["1h"]), Math.round(data.wind.speed)];
     displayInfo(presArr); 
     displayForecast(forecast, presArr);
 }
@@ -18,6 +19,7 @@ function displayWeather(city) {
     const unixTimestamp = city.dt + city.timezone;
     const dateObj = new Date(unixTimestamp * 1000);
     const utcString = dateObj.toUTCString();
+    this.timeZone = city.timezone / 3600;
     const hour = utcString.slice(-12, -10);
     const time = (hour >= 9 && hour < 23) ? "day" : "night";
 
@@ -55,13 +57,19 @@ function displayInfo(arr) {
     document.getElementById("precipitation").textContent = arr[1] + " mm";
     document.getElementById("wind").textContent = arr[2] + " mph";
 }
-function displayForecast(next24Hours, arr) {
+function displayForecast(forecast, curentArr) {
     document.querySelector(".hourly-forecast").innerHTML = "";
     const descArr = [];
     let id = 0;
-    next24Hours.forEach(element => {
-        const time = (parseInt(element.dt_txt.slice(-8, -6)) >= 9 && parseInt(element.dt_txt.slice(-8, -6)) < 23) ? "day" : "night";
-        const hour = timeConverter(parseInt(element.dt_txt.slice(-8, -6)));
+    forecast.forEach(element => {
+        const hour = timeConverter(parseInt(element.dt_txt.slice(-8, -6))+timeZone);
+        let time; 
+        if (hour === "9:00 AM" || hour === "10:00 AM" || hour === "11:00 AM")
+            time = "day";
+        else if (hour.slice(-2) === "PM" && hour.slice(0, -6) !== "11")
+            time = "day";
+        else
+            time = "night";
 
         const temperature = Math.round((element.main.temp-273)*(9/5)+32);
         const weather = element.weather[0].main.toLowerCase();
@@ -99,15 +107,29 @@ function displayForecast(next24Hours, arr) {
         div.append(p2);
 
         div.addEventListener("click", event => {
-            blur(event, descArr[event.target.id]);
+            blur(event, descArr[event.target.id], "blur(3px)");
         });
         div.addEventListener("mouseout", event => {
-            unBlur(event, arr);
+            blur(event, curentArr, "blur(0px)");
         });
     });
 
     function timeConverter(hour) {
-        const time = (hour > 12) ? `${hour-12}:00 PM` : ((hour === 0) ? `12:00 AM` : `${hour}:00 AM`);
+        if (hour <= 0)
+            hour+=24;
+        else if (hour > 24)
+            hour-=24;
+
+        let time;
+        if (hour > 12 && hour !== 24)
+            time = hour-12 + ":00 PM";
+        else if (hour === 12)
+            time = "12:00 PM"
+        else if (hour < 12)
+            time = hour + ":00 AM";
+        else if (hour === 24)
+            time = "12:00 Am"
+
         return time;
     }
 }
@@ -120,22 +142,13 @@ const eventListeners = () => {
         }            
     });   
 }
-function blur(event, arr) {
-    document.querySelector("#info").style.filter = "blur(3px)";
-    document.querySelector("#card").style.background.filter = "blur(3px)";
+function blur(event, arr, blur) {
+    document.querySelector("#info").style.filter = blur;
+    // document.querySelector("#card").style.background.filter = "blur(3px)";
     for (let i=0; i<8; i++) {
         if (i == event.target.id)
             ++i;
-        document.getElementById(`${i}`).style.filter = "blur(3px)";
+        document.getElementById(`${i}`).style.filter = blur;
     }
    displayInfo(arr);
-}
-function unBlur(event, arr) {
-    document.querySelector("#info").style.filter  = "blur(0)";
-    for (let i=0; i<8; i++) {
-        if (i == event.target.id)
-            ++i;
-        document.getElementById(`${i}`).style.filter = "blur(0)";
-    }
-    displayInfo(arr);
 }
